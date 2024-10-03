7afdb1869ee5f51704f415ece58a8629125204a6
Axel Castro <86852111+AxelCastr00@users.noreply.github.com>
2024-10-03 00:39:09 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,3 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
       form.reset(); // Reiniciar el formulario
   });
 });
+
+// script.js
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); // Activa o desactiva la clase
+});
