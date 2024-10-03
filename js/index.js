a948fc44d4ddea82b546cc8400fa26215f6a4982
Axel Castro <86852111+AxelCastr00@users.noreply.github.com>
2024-10-03 02:13:16 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-// scripts.js
-
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-
-  // Manejo del envío del formulario
-  form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Evitar el envío real del formulario
-    alert('Mensaje enviado. ¡Gracias por contactarnos!');
-    form.reset(); // Reiniciar el formulario
-  });
-});
 
 // Manejo del menú desplegable
 document.getElementById('hamburger').addEventListener('click', function() {
