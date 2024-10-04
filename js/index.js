7e5104c5189d46f06999d08d8b8395ef97a40282
Axel Castro <86852111+AxelCastr00@users.noreply.github.com>
2024-10-03 22:07:49 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // Manejo del menú desplegable
 document.getElementById('hamburger').addEventListener('click', function() {
   const navLinks = document.querySelector('.nav-links');
@@ -9,13 +8,26 @@ document.getElementById('hamburger').addEventListener('click', function() {
   hamburgerIcon.classList.toggle('active');
 });
 
-// Asegurarse de que el menú se cierre al hacer clic en un enlace
+// Asegurarse de que el menú se cierre al hacer clic en cualquier enlace
 const navLinks = document.querySelectorAll('.nav-links li a');
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
-    const navLinksContainer = document.querySelector('.nav-links');
-    navLinksContainer.classList.remove('active'); // Cierra el menú
-    const hamburgerIcon = document.getElementById('hamburger').querySelector('i');
-    hamburgerIcon.classList.remove('active'); // Cambia el ícono de cruz a hamburguesa
+      const navLinksContainer = document.querySelector('.nav-links');
+      navLinksContainer.classList.remove('active'); // Cierra el menú
+      const hamburgerIcon = document.getElementById('hamburger').querySelector('i');
+      hamburgerIcon.classList.remove('active'); // Cambia el ícono de cruz a hamburguesa
   });
+});
+
+// Cerrar el menú si se hace clic fuera de él
+document.addEventListener('click', (event) => {
+  const menuToggle = document.getElementById('hamburger');
+  const navLinksContainer = document.querySelector('.nav-links');
+
+  // Verifica si el clic fue fuera del menú y el botón
+  if (!menuToggle.contains(event.target) && !navLinksContainer.contains(event.target)) {
+      navLinksContainer.classList.remove('active'); // Cierra el menú
+      const hamburgerIcon = menuToggle.querySelector('i');
+      hamburgerIcon.classList.remove('active'); // Cambia el ícono de cruz a hamburguesa
+  }
 });
